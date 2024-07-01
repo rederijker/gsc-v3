5a5cab6f7af08868d0216c01c09b20780de93006
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-07-02 01:06:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -903,7 +903,7 @@ def authorize_app():
 
     if st.session_state.credentials is None:
         auth_url, _ = flow.authorization_url(prompt='consent', access_type='offline')
-        st.markdown(f"""
+         st.markdown(f"""
             <div class="header" style="padding:5%;">
             <h1 style="text-align:center;">SEO Gnosis</h1>
             <h2 style="text-align:center;">Master Google Search Console Data like a Pro with a Free SEO tool</h2>
@@ -911,13 +911,14 @@ def authorize_app():
             </div>
             <div style="text-align:center;">
                 <a href="{auth_url}" style="text-decoration:none;">
-                    <button style="background-color: #4285F4; color: white; border: none; padding: 10px 20px; font-size: 17px; border-radius: 5px; cursor: pointer;">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/120px-Google_%22G%22_logo.svg.png" alt="Google logo" style="width: 20px; height: 20px; vertical-align: middle; margin-right: 8px;">
+                    <button style="background-color: white; color: #4285F4; border: 1px solid #4285F4; padding: 10px 20px; font-size: 17px; border-radius: 5px; cursor: pointer; display: inline-flex; align-items: center;">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google logo" style="width: 20px; height: 20px; margin-right: 8px;">
                         Login with Google
                     </button>
                 </a>
             </div>
             """, unsafe_allow_html=True)
+
 
             
     return st.session_state.credentials
