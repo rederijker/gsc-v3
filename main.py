7c477affe1ff098738a9590cf6ea22ae390af59c
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-07-02 00:30:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -936,8 +936,13 @@ def fetch_data_chunk(webmasters_service, site_url, start_date, end_date, dimensi
 
 
 with st.container(border=True):
-    st.header('SEO Gnosis')
-    st.write("Welcome to SEO Gnosis! Explore and analyze Google Search Console data with ease. Authenticate effortlessly, choose your website, and access features like URL inspection and search analytics. Customize your searches and enjoy unlimited rows of data, without the need for programming skills. Unleash the full potential of your website's visibility with this user-friendly tool, perfect for webmasters, SEO experts, and digital marketers.")
+    st.markdown("""
+                <h1 style="text-align:center;">SEO Gnosis</h1>
+                <h2>Master Google Search Console Data like a Pro with a Free SEO tool</h2>
+                """, unsafe_allow_html=True)
+
+st.header('SEO Gnosis')
+st.write("Welcome to SEO Gnosis! Explore and analyze Google Search Console data with ease. Authenticate effortlessly, choose your website, and access features like URL inspection and search analytics. Customize your searches and enjoy unlimited rows of data, without the need for programming skills. Unleash the full potential of your website's visibility with this user-friendly tool, perfect for webmasters, SEO experts, and digital marketers.")
 st.markdown("""
     <style>
     .st-emotion-cache-qcpnpn.e1f1d6gn0 {
