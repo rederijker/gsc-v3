969afb4100d36b9d37d579dccbacf59450420cc2
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-07-03 00:32:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1083,16 +1083,14 @@ def authorize_app():
         st.markdown(f"""
         <h1 style="text-align:center;">SEO Gnosis</h1>""",
         unsafe_allow_html=True
-        )
-        st.write("Auth code received:", auth_code)  # Debug: Visualizza il codice di autorizzazione
-        
+        )        
         if not st.session_state.credentials:
             
             try:
                 flow.fetch_token(code=auth_code)
                 credentials = flow.credentials
                 st.session_state.credentials = credentials
-                st.write("✅ You are connected with Google Search Console API")
+                st.write("✅ Auth code received:. Now you are connected with Google Search Console API")
             except Exception as e:
                 st.write(f"Error during authorization: {e}")
                 st.write(f"Please reauthenticate")
