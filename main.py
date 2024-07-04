b75de306f9fec98362aa8876a9bd6c749305ee85
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-07-04 23:21:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1972,7 +1972,7 @@ if st.session_state.data_loaded:
                                 st.error("The DataFrame does not contain the required column 'Keyword'. Please check the data processing.")
                             else:
                                 col1, col2, col3, col4, col5 = st.columns([2, 1, 1, 1, 1])
-                                with col1;
+                                with col1:
                                     # Gestione dello stato del filtro di ricerca per query
                                     search_query = st.text_input(
                                         label="",
