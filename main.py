4f25b0aa5e99b66244922c574d0e53206b7f0255
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-07-20 01:21:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1267,7 +1267,7 @@ if credentials:
     
                 with st.spinner("Inspecting URLs..."):
                     # Uso di ThreadPoolExecutor per l'esecuzione concorrente
-                    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+                    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
                         future_to_url = {executor.submit(inspect_url, url, st.session_state.selected_site): url for url in urls}
                         for idx, future in enumerate(concurrent.futures.as_completed(future_to_url)):
                             url = future_to_url[future]
