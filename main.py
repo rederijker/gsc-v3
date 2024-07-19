18d4545f8941f271188f3c5290bb6bb9e78aa512
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-07-20 01:09:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1254,7 +1254,6 @@ if credentials:
     tab1, tab2 = st.tabs(["SEARCH ANALYTICS", "BULK URLs INSPECTION"])
 
     with tab1:
-    with tab2:
         urls_to_inspect = st.text_area("Insert URLs to inspect (one per line):", height=200)
         if st.button('URL INSPECTION 🕵️‍♂️'):
             if st.session_state.selected_site:
