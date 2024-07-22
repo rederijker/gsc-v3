2fa22a33b66afc7f8fbadcdee9a6cc84864721ff
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-07-22 21:58:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2329,8 +2329,7 @@ if credentials:
     
                 start_time = time.time()  # Inizio del timer
     
-                with st.spinner("Inspecting URLs..."):
-                    time.sleep(1)
+                with st.status("Inspecting URLs..."):
                     # Uso di ThreadPoolExecutor per l'esecuzione concorrente
                     with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:  # Limita a 1 worker thread
                         future_to_url = {executor.submit(inspect_url, url, st.session_state.selected_site): url for url in urls}
