721501e80c548ed7cf49dffd63205e7cfd7e682d
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-07-23 22:06:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1142,7 +1142,7 @@ def authorize_app():
 
     if auth_code:
         st.markdown(f"""
-        <h1 style="text-align:center;">SEO Gnosis</h1>""",
+        <h1 style="text-align:center;">GSC InsightHub</h1>""",
         unsafe_allow_html=True
         )        
         if not st.session_state.credentials:
@@ -1161,17 +1161,9 @@ def authorize_app():
         gif_url = "https://github.com/rederijker/gsc-v3/blob/main/assets/back.gif?raw=true"
         st.markdown(f"""
         <div class="header" style="padding:5%;background-image:url({gif_url});">
-        <h1 style="text-align:center;">SEO Gnosis</h1>
-        <h2 style="text-align:center;">Master Google Search Console Data like a Pro with a Free SEO tool</h2>
-        <p style="text-align:center;font-size:17px;">Explore <strong>Google Search Console data</strong>, generate detailed reports, customize searches, and access unlimited information without programming skills required for <strong>Google Search Console API</strong>. Perfect for webmasters, SEO experts, and digital marketers.
-
-
-
-
-
-
-.</p>
-        
+        <h1 style="text-align:center;">GSC InsightHub</h1>
+        <h2 style="text-align:center;">Master Google Search Console Data like a Free SEO tool</h2>
+        <p style="text-align:center;font-size:17px;">Explore <strong>Google Search Console data</strong>, generate detailed reports, customize searches, and access unlimited information without programming skills required for <strong>Google Search Console API</strong>. Perfect for webmasters, SEO experts, and digital marketers.</p>
         <div style="text-align:center;">
             <a href="{auth_url}" style="text-decoration:none;">
                 <button style="background-color: white; color: #4285F4; border: 1px solid #4285F4; padding: 10px 20px; font-size: 17px; border-radius: 5px; cursor: pointer; display: inline-flex; align-items: center;">
