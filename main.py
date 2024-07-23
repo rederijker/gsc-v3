d95a1cb370eb268855ecf03062a4c35a367dce22
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-07-23 22:07:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1162,7 +1162,7 @@ def authorize_app():
         st.markdown(f"""
         <div class="header" style="padding:5%;background-image:url({gif_url});">
         <h1 style="text-align:center;">GSC InsightHub</h1>
-        <h2 style="text-align:center;">Master Google Search Console Data like a Free SEO tool</h2>
+        <h2 style="text-align:center;">Master Google Search Console Data like a Pro with a Free SEO tool</h2>
         <p style="text-align:center;font-size:17px;">Explore <strong>Google Search Console data</strong>, generate detailed reports, customize searches, and access unlimited information without programming skills required for <strong>Google Search Console API</strong>. Perfect for webmasters, SEO experts, and digital marketers.</p>
         <div style="text-align:center;">
             <a href="{auth_url}" style="text-decoration:none;">
