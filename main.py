b0d92237d8bceea0c19a6471c412ae4942ad51cf
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-08-29 00:04:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2441,23 +2441,23 @@ if credentials:
             st.session_state['urls'] = ""
         if 'json_file' not in st.session_state:
             st.session_state['json_file'] = None
-        
-        # Seleziona l'azione desiderata
-        st.subheader("Step 1: Seleziona l'azione desiderata")
-        st.session_state['action'] = st.selectbox(
-            "Azione", 
-            ["Aggiorna URL", "Rimuovi URL", "Conoscere lo stato dell'URL"], 
-            index=["Aggiorna URL", "Rimuovi URL", "Conoscere lo stato dell'URL"].index(st.session_state['action'])
-        )
-        
-        # Input URL
-        st.subheader("Step 2: Inserisci gli URL (uno per riga)")
-        st.session_state['urls'] = st.text_area("Inserisci gli URL:", st.session_state['urls'])
-        
-        # Caricamento del file JSON delle credenziali
-        st.subheader("Step 3: Carica il file JSON delle credenziali Google Cloud")
-        st.session_state['json_file'] = st.file_uploader("Carica il file JSON", type=["json"])
-        
+        st.columns(2)
+        with col1:
+            # Seleziona l'azione desiderata
+            
+            st.session_state['action'] = st.selectbox(
+                "Azione", 
+                ["Aggiorna URL", "Rimuovi URL", "Conoscere lo stato dell'URL"], 
+                index=["Aggiorna URL", "Rimuovi URL", "Conoscere lo stato dell'URL"].index(st.session_state['action'])
+            )
+        with col2:
+            # Input URL
+            st.session_state['urls'] = st.text_area("Inserisci gli URL:", st.session_state['urls'])
+            
+            # Caricamento del file JSON delle credenziali
+            st.subheader("Step 3: Carica il file JSON delle credenziali Google Cloud")
+            st.session_state['json_file'] = st.file_uploader("Carica il file JSON", type=["json"])
+            
         if st.button("Esegui"):
             if not st.session_state['urls'] or not st.session_state['json_file']:
                 st.error("Assicurati di aver inserito gli URL e caricato il file JSON.")
