15b5ee613449540d5b6c32c0d8208a3497599728
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-09-14 23:00:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2088,12 +2088,14 @@ if credentials:
                             st.session_state.selected_page_on_page = selected_page_on_page
                     
                             # Creazione di una barra di progresso
-                            progress_bar = st.progress(0)
+                            progress_text = "Fetching page data..."
+                            progress_bar_2 = st.progress(0, text=progress_text)
                             
                             # Simulazione del caricamento dei dati con un ciclo
                             for percent_complete in range(100):
-                                time.sleep(0.05)  # Simula il tempo necessario per caricare i dati
-                                progress_bar.progress(percent_complete + 1)
+                                time.sleep(0.02)  # Simula il tempo necessario per caricare i dati
+                                progress_bar_2.progress(percent_complete + 1)
+                                progress_bar_2.empty()
                     
                             st.session_state.page_data = fetch_page_data(selected_page_on_page)
                             st.session_state.keyword_analysis = None
