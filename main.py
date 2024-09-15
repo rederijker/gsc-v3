073bd6f1f14f4a8a240fc4804c3b91c27610e785
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-09-15 11:46:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1429,10 +1429,11 @@ if credentials:
 
                     
             # 2. horizontal menu
-            with sidebar.selected2 = option_menu(None, ["WEBSITE DATA OVERVIEW", "QUERIS REPORT", "PAGES REPORT", "PAGE OPTIMIZATION", "QUERIES GROUPER"], 
-                icons=['house', 'cloud-upload', 'cloud-upload', "list-task", 'gear'], 
-                menu_icon="cast", default_index=0, orientation="horizontal")
-            selected2
+            with sidebar:
+                selected2 = option_menu(None, ["WEBSITE DATA OVERVIEW", "QUERIS REPORT", "PAGES REPORT", "PAGE OPTIMIZATION", "QUERIES GROUPER"], 
+                    icons=['house', 'cloud-upload', 'cloud-upload', "list-task", 'gear'], 
+                    menu_icon="cast", default_index=0, orientation="horizontal")
+                selected2
 
 
 
