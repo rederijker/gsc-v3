e6d985544c8f263b274ac40a9b832a57637f7d1e
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-09-15 11:23:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1574,6 +1574,8 @@ if credentials:
             
             
             elif selected2 == "QUERIS REPORT":
+                if st.session_state.data_loaded:
+                    df = st.session_state.df
             
                 with st.container(border=True):
                     st.subheader("1. Queries Performance Report")
