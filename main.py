7387a6b0873bac0856f4c712a44e5a4a2ea6f1b6
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-09-16 22:58:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1698,7 +1698,7 @@ if credentials:
                         with col2:
                           
                             #Suddividere i dati in quattro DataFrame in base ai quadranti specificati e fornire all'utente la lista delle query in ciascun quadrante
-                            rupper_high_ctr = df[(df['Position'] > average_position) & (df['CTR'] > average_ctr)]
+                            upper_high_ctr = df[(df['Position'] > average_position) & (df['CTR'] > average_ctr)]
 
                             
                             lower_high_ctr = df[(df['Position'] > average_position) & (df['CTR'] > average_ctr)]
@@ -1718,7 +1718,7 @@ if credentials:
                                 }
                         
                                 # Raggruppiamo e aggreghiamo i DataFrame dei quadranti
-                                df_upper_high_ctr = rupper_high_ctr.groupby('Query').agg(agg_funcs2).reset_index()
+                                df_upper_high_ctr_2 = upper_high_ctr.groupby('Query').agg(agg_funcs2).reset_index()
                                 df_lower_high_ctr = lower_high_ctr.groupby('Query').agg(agg_funcs2).reset_index()
                                 df_lower_low_ctr = lower_low_ctr.groupby('Query').agg(agg_funcs2).reset_index()
                                 df_upper_low_ctr = upper_low_ctr.groupby('Query').agg(agg_funcs2).reset_index()
@@ -1728,7 +1728,7 @@ if credentials:
                                 st.markdown("<br><br>", unsafe_allow_html=True)
                                 with st.expander(":green[GREEN QUADRANT: Queries with Above-Average Position and CTR]"):
                                     st.write("Queries with CTR and position equal or grather then the average")
-                                    st.write(df_upper_high_ctr)    
+                                    st.write(df_upper_high_ctr_2)    
                                     st.write(f"Upper High CTR (Green Quadrant): {upper_high_ctr.shape[0]} queries")
 
                                     
