9063f6c191c869c967020bd68d23f140182319a5
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-09-16 23:14:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1698,7 +1698,7 @@ if credentials:
                         with col2:
                           
                             #Suddividere i dati in quattro DataFrame in base ai quadranti specificati e fornire all'utente la lista delle query in ciascun quadrante
-                            upper_high_ctr = df[(df['Position'] > average_position) & (df['CTR'] > average_ctr)]                            
+                            upper_high_ctr = df[(df['Position'] <= average_position) & (df['CTR'] > average_ctr)]                            
                             lower_high_ctr = df[(df['Position'] > average_position) & (df['CTR'] > average_ctr)]
                             lower_low_ctr = df[(df['Position'] > average_position) & (df['CTR'] <= average_ctr)]
                             upper_low_ctr = df[(df['Position'] <= average_position) & (df['CTR'] <= average_ctr)]
@@ -1729,6 +1729,9 @@ if credentials:
                                     st.write(df_upper_high_ctr)    
                                     st.write(f"Min CTR: {min_ctr}, Max CTR: {max_ctr}, Average CTR: {average_ctr}")
                                     st.write(f"Min Position: {min_position}, Max Position: {max_position}, Average Position: {average_position}")
+
+                                    st.write(f"Queries con CTR > {average_ctr} e Position <= {average_position}: {df_query_performance[(df_query_performance['CTR'] > average_ctr) & (df_query_performance['Position'] <= average_position)].shape[0]}")
+
 
 
                                     
