42606d8d4fde0716430d772c12cb478df55cb7d9
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-09-16 22:46:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1697,7 +1697,7 @@ if credentials:
                             fig.show()
                         with col2:
                             # Suddividere i dati in quattro DataFrame in base ai quadranti specificati e fornire all'utente la lista delle query in ciascun quadrante
-                            upper_high_ctr = df[(df['Position'] >= average_position) & (df['CTR'] > average_ctr)]
+                            upper_high_ctr = df[(df['Position'] <= average_position) & (df['CTR'] > average_ctr)]
                             lower_high_ctr = df[(df['Position'] > average_position) & (df['CTR'] > average_ctr)]
                             lower_low_ctr = df[(df['Position'] > average_position) & (df['CTR'] <= average_ctr)]
                             upper_low_ctr = df[(df['Position'] <= average_position) & (df['CTR'] <= average_ctr)]
