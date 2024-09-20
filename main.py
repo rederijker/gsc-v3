dd4e55b652e21fef3265b3d67ecdd35c4e262b5d
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-09-21 00:25:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1713,6 +1713,10 @@ if credentials:
                                 }
                                 
                                 df_grouped = df.groupby('Query').agg(agg_funcs2).reset_index()
+                                df_grouped['CTR'] = df_grouped['ctr'].apply(lambda x: f'{x:.2f}%')
+
+
+                                
                                 ctr_mean_df_grouped = df_grouped["CTR"].mean()
                                 position_mean_df_grouped = df_grouped["Position"].mean()
 
