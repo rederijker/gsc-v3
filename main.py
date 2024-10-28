c69395fd774359f894fe4b4ec265615284c8af8c
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-10-29 00:46:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1899,10 +1899,10 @@ if credentials:
                             # Calcolare la posizione media per ogni query
                             df_query_page_serp1 = df.copy()
                             df_query_page_serp = df_query_page_serp1[
-                                ~((df_query_page_serp1['Impressions'] == 1) & 
-                                (df_query_page_serp1['Clicks'] == 0))  # Escludi solo le query con 1 impressione e 0 clic
+                                ~((df_query_page_serp1['Impressions'] == 1) & (df_query_page_serp1['Clicks'] == 0)) &  # Escludi 1 impressione e 0 clic
+                                ~((df_query_page_serp1['Clicks'] == 1) & (df_query_page_serp1['CTR'] == 1) & (df_query_page_serp1['Position'] == 1) & (df_query_page_serp1['Impressions'] == 1))  # Escludi anche le query con solo 1 impressione
                             ]
-           
+                                       
                             df_query_avg_position = df_query_page_serp.groupby('Query')['Position'].mean().reset_index()
                             df_query_avg_position.rename(columns={'Position': 'Avg_Position'}, inplace=True)
                 
