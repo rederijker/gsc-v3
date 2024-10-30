257eeee5d839f959e446f01baddf7030b3d6e75a
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-10-30 22:55:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1912,7 +1912,7 @@ if credentials:
                             # Rimuovere i duplicati dalle query basandosi sulla combinazione di 'Query' e 'SERP_Page'
                             df_query_performance_unique1 = df_query_page_serp.drop_duplicates(subset=['Query', 'SERP_Page'])
                             df_query_performance_unique =  df_query_performance_unique1[
-                                                                                        ~(df_query_performance_unique1['Impressions'] < 10)
+                                                                                        ~ (df_query_performance_unique1['Impressions'] < 10)
                                                                                         ]                                                                                     ]
                 
                             # Conta il numero totale di query uniche
