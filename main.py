4a34d3820a667bbcec9df1a8074d27bdf37cff9a
Cristiano Caggiula <67266943+rederijker@users.noreply.github.com>
2024-11-04 20:31:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1967,9 +1967,7 @@ if credentials:
                             df_filtered_page = df_query_performance_unique[df_query_performance_unique['SERP_Page'] == selected_page]
                             st.write(f"Query details for {selected_page}")
                             st.dataframe(df_filtered_page[['Query', 'Avg_Position', 'Clicks', 'Impressions', 'CTR']])
-                        else:
-                            st.warning("Add the necessary columns (Query, Page, Position, Clicks, Impressions, CTR) to generate the 2. Queries distribution on SERP Pages Report")
-
+              
             
                 try:
                     # Controllo se la colonna 'Page' è presente
